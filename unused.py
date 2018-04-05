fc6f5dfcfd43497e12765b5251b09e586b943fce
Guido Rugo <guido.rugo@gmail.com>
2018-04-05 18:33:14 -0300

    V2.0 BETA

--- unused.py
+++ unused.py
@@ -1,245 +1,120 @@
 #!/usr/bin/python2.7
+
 import boto3
-import sys
-import os
-import getopt
-import botocore.exceptions
+from prettytable import PrettyTable
+from operator import itemgetter
+from fnmatch import fnmatch, fnmatchcase
 import argparse
-import re
 
-# ToDo
-#  - Multithreading / Multiprocessing
-#  - STS
-
-profiles = None
 parser = argparse.ArgumentParser()
-parser.add_argument('--all', '-a', action='store_true', help='Show all')
-args = parser.parse_args()
+parser.add_argument('--profile', '-p', metavar='<name>', nargs='?', help='Specify profile')
+parser.add_argument('--region', '-r', metavar='<us-east-1>,<eu-west-1>,...', nargs='?', help='Specify region')
+parser.add_argument('--service', '-s', metavar='<ec2|rds|s3|all>', nargs='?', help='Service(s) to scrape')
+args   = parser.parse_args()
 
-def test_conn(args):
-   try:
-      ec2 = boto3.client('ec2')
-      regions = [region['RegionName'] for region in ec2.describe_regions()['Regions']]
-      s3 = boto3.resource('s3')
-      test = s3.buckets.all()
-      for region in regions:
-         ec2 = boto3.client('ec2', region_name=region)
-   except botocore.exceptions.ClientError as ex:
-      if ex.response['Error']['Code'] == 'ExpiredToken':
-         print('The token seems expired.')
-         sys.exit(1)
-      elif ex.response['Error']['Code'] == 'AccessDenied':
-         print('\nAccess denied')
-         sys.exit(1)
+def list_ec2():
+   if args.profile:
+      profiles = args.profile.split()
+   else:
+      profiles = boto3.Session().available_profiles
+   for profile in profiles:
+      count = 0
+      if (not fnmatch(profile, 'default')) and (not fnmatch(profile, '*assumed*')) and not None:
+         #print ('\nProcessing profile '+profile)
+         boto3.setup_default_session(profile_name=profile) 
+         if not args.region:
+            ec2 = boto3.client('ec2', region_name='us-east-1') 
+            regions = [region['RegionName'] for region in boto3.client('ec2', region_name='us-west-1').describe_regions()['Regions']]
+         else:
+            ec2 = boto3.client('ec2', region_name=args.region)
+            regions = args.region.split()
+         ec2_table = PrettyTable(['\033[33mName\033[0m', '\033[33mInstance ID\033[0m', '\033[33mRegion\033[0m', '\033[33mInstance Type\033[0m', '\033[33mPublic IP\033[0m', '\033[33mState\033[0m'])
+         for region in regions:
+            ec2 = boto3.resource('ec2', region_name=region)
+            instances = ec2.instances.filter()
+            for instance in instances:
+               if instance is not None:
+                  if instance.state['Name'] == 'pending':
+                     state = '\033[33mPending\033[0m'
+                  elif instance.state['Name'] == 'running':
+                     state = '\033[32mRunning\033[0m'
+                  elif instance.state['Name'] == 'shutting-down':
+                     state = '\033[91mShutting down\033[0m'
+                  elif instance.state['Name'] == 'terminated':
+                     state = '\033[31mTerminated\033[0m'
+                  elif instance.state['Name'] == 'stopping':
+                     state = '\033[34mStopping\033[0m'
+                  elif instance.state['Name'] == 'stopped':
+                     state = '\033[35mStopped\033[0m'
+                  if instance.tags is not None:
+                     for tag in instance.tags:                                                                     
+                        if 'Name' in tag['Key'] or 'name' in tag['Key']:
+                           name = tag['Value']
+                        else:
+                           name = '-'  
+                  ec2_table.add_row([name, instance.instance_id, region, instance.instance_type, instance.public_ip_address, state])
+                  count += 1
+      if count > 0:
+         print('\n\033[93mTables in \033[0m'+profile)
+         print ec2_table.get_string(sortby='\033[33mState\033[0m')
+      else: 
+         print('\nNo instances on '+profile)
+
+def show_ip():
+   if args.profile:
+      profiles = args.profile.split()
+   else:
+      profiles = boto3.Session().available_profiles
+   for profile in profiles:
+      count = 0
+      if (not fnmatch(profile, 'default')) and (not fnmatch(profile, '*assumed*')) and not None:
+         print ('\nProcessing profile '+profile)
+         boto3.setup_default_session(profile_name=profile) 
+         if not args.region:
+            ec2 = boto3.client('ec2', region_name='us-east-1') 
+            regions = [region['RegionName'] for region in boto3.client('ec2', region_name='us-west-1').describe_regions()['Regions']]
+         else:
+            ec2 = boto3.client('ec2', region_name=args.region)
+            regions = args.region.split()
+         ips_table = PrettyTable(['\033[33mInstance ID\033[0m', '\033[33mPublic IP\033[0m', '\033[33mRegion\033[0m', '\033[33mStatus\033[0m'])
+   #print('\n\033[32mShowing Elastic IPs unused\033[0m\n')
+         for region in regions:
+            for address in boto3.client('ec2', region_name=region).describe_addresses()['Addresses']:
+               if 'NetworkInterfaceId' in address:
+                  status = '\033[32mIn used\033[0m'
+               else:
+                  status = '\033[31mUnused\033[0m'
+               #print(address['PublicIp']+"\033[32m in \033[0m"+region)
+               if 'InstanceId' not in address:
+                  instanceid = '-'
+               else:
+                  instanceid = address['InstanceId']
+               ips_table.add_row([instanceid, address['PublicIp'], region, status])
+               count += 1
+      if count > 0:
+         print ('\033[93mTables in \033[0m'+profile)
+         print ips_table.get_string(sortby='\033[33mStatus\033[0m')
       else:
-         print('Error unknown. Error : ' + ex.response['Error']['Code'])
-         sys.exit(1)
-   except botocore.exceptions.NoRegionError:   
-      print('\n\033[31mCould not find REGION or DEFAULT_REGION in OS environs.\nWill use "\033[0mus-west-1\033[31m" as default.\033[0m')
-      os.environ['AWS_DEFAULT_REGION'] = 'us-west-1'
-      os.environ['AWS_REGION'] = 'us-west-1'
-   except botocore.exceptions.NoCredentialsError:
-      print('\033[31mIt seems your credentials are missing.\033[0m\n')
+         print('\nNo Elastic IPs on '+profile)
 
-def main(args):
-  if len(sys.argv) == 1:
-      print('\n\033[32mMenu\033[0m\n') 
-      menu()
-  elif args.all:
-         from fnmatch import fnmatch, fnmatchcase
-         show_credentials(args)
-         list_profiles()
-         for profile in boto3.Session().available_profiles:
-            if (not fnmatch(profile, 'default')) and (not fnmatch(profile, '*assumed*')):
-               boto3.setup_default_session(profile_name=profile)
-               show_everything(args)
+def main():
+   if args.service == 'ec2':
+      list_ec2()
+   elif args.service == 'eip':
+      show_ip()
+   #elif args.service == 'ec2':
+   #   scrape_ec2(region, args.label)
+   #elif args.service == 'rds':
+   # scrape_rds(region, args.label)
+   #elif args.service == 's3':
+   # scrape_s3(region, args.label)
+   #elif args.service == 'all':
+   # scrape_ebs(region, args.label)
+   # scrape_ec(region, args.label)
+   # scrape_ec2(region, args.label)
+   # scrape_rds(region, args.label)
+   # scrape_s3(region, args.label)
+   #else:
+   # exit('Service to scrape not valid or not defined: "{0}".'.format(args.service))
 
-def menu():
-   menu = {'1':'[\033[33m1\033[0m] Show everything','2':'[\033[33m2\033[0m] Show credentials','3':'[\033[33m3\033[0m] Show instances stopped','4':'[\033[33m4\033[0m] Show buckets','5':'[\033[33m5\033[0m] Show unused IPs','6':'[\033[33m6\033[0m] Show Elastic LoadBalancer unused'}
-   while True: 
-     for key, value in (sorted(menu.iteritems())): 
-       print(value)
-     print('')
-     selection = raw_input('Select : ') 
-     if selection == '1': 
-       show_credentials(args)
-       list_profiles()
-       show_everything(args)
-     elif selection == '2':
-       show_credentials(args)
-       list_profiles()
-     elif selection == '3': 
-      from fnmatch import fnmatch, fnmatchcase
-      for profile in boto3.Session().available_profiles:
-         if (not fnmatch(profile, 'default')) and (not fnmatch(profile, '*assumed*')):
-            print('\033[93mAWS profile \033[0m' + profile)
-            boto3.setup_default_session(profile_name=profile)
-            show_instances(args)
-            instances_temp(args)
-     elif selection == '4':
-      from fnmatch import fnmatch, fnmatchcase
-      for profile in boto3.Session().available_profiles:
-         if (not fnmatch(profile, 'default')) and (not fnmatch(profile, '*assumed*')):
-            print('\033[93mAWS profile \033[0m' + profile)
-            boto3.setup_default_session(profile_name=profile)
-            show_buckets(args)
-     elif selection == '5':
-      from fnmatch import fnmatch, fnmatchcase
-      for profile in boto3.Session().available_profiles:
-         if (not fnmatch(profile, 'default')) and (not fnmatch(profile, '*assumed*')):
-            print('\033[93mAWS profile \033[0m' + profile)
-            boto3.setup_default_session(profile_name=profile)
-            show_ip(args)
-     elif selection == '6':
-      from fnmatch import fnmatch, fnmatchcase
-      for profile in boto3.Session().available_profiles:
-         if (not fnmatch(profile, 'default')) and (not fnmatch(profile, '*assumed*')):
-            print('\033[93mAWS profile \033[0m' + profile)
-            boto3.setup_default_session(profile_name=profile)
-            show_elb(args)
-     else: 
-       print('Unknown option selected. Exiting...')
-       sys.exit(0) # Exiting without errors
-
-def show_everything(args):
-      from fnmatch import fnmatch, fnmatchcase
-      for profile in boto3.Session().available_profiles:
-         if (not fnmatch(profile, 'default')) and (not fnmatch(profile, '*assumed*')):
-            print('\033[93mAWS profile \033[0m' + profile)
-            boto3.setup_default_session(profile_name=profile)
-            show_instances(args)
-            instances_temp(args)
-            show_ip(args)
-            show_elb(args)
-            show_buckets(args)
-
-def show_credentials(args):
-   try:
-      print('\n\033[32mCredentials to be used :\033[0m\n')
-      print('\033[31mAWS access key\033[0m ' + boto3.Session().get_credentials().access_key) 
-      print('\033[31mAWS secret key\033[0m ' + boto3.Session().get_credentials().secret_key)
-      print('\033[31mAWS seesion token\033[0m ' + boto3.Session().get_credentials().token)
-      print('\033[31mAWS region name\033[0m ' + boto3.Session().region_name)
-   except KeyError: # Do not explode if environs could not be loaded
-      print('AWS config not found. Cannot continue without this\n')
-      if args.mail is not None:
-         print('\033[31mDestination mail address\033[0m ' + args.mail)
-      sys.exit(1)
-   print('')
-
-def show_instances(args):
-   try:
-      ec2 = boto3.client('ec2')
-      regions = [region['RegionName'] for region in ec2.describe_regions()['Regions']]
-   except botocore.exceptions.ClientError:
-      print('\n\033[31mToken expired. Sorry :(\033[0m')
-      sys.exit(1)
-   print('\n\033[32mShowing stopped instances\033[0m\n')
-   for region in regions: 
-      conection = boto3.resource('ec2', region_name=region)
-      instances = conection.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['stopped','terminated']}])
-      for instance in instances:
-         if instance is not None:
-            if instance.tags is not None:
-               for tag in instance.tags:
-                  if tag['Key'] == 'Name':
-                     print('\033[32mName tag    \033[0m'+tag['Value'])
-               print('\033[32mID          \033[0m'+instance.id)
-               print('\033[32mType        \033[0m'+instance.instance_type)
-               if instance.public_dns_name:
-                  print('\033[32mPublic DNA  \033[0m'+instance.public_dns_name)
-               print('\033[32mRegion      \033[0m'+region)
-               print('---------------------------------------')
-
-def instances_temp(args):
-   print('\n\033[32mShowing "tmp" / "temp" / "test" running instances\033[0m\n')
-   try:
-      ec2 = boto3.client('ec2')
-      regions = [region['RegionName'] for region in ec2.describe_regions()['Regions']]
-   except botocore.exceptions.ClientError:
-      print('\n\033[31mToken expired. Sorry :(\033[0m')
-      sys.exit(1)
-   for region in regions:
-      conection = boto3.resource('ec2', region_name=region)
-      instances = conection.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']},{'Name': 'tag:Name', 'Values': ['*tmp*']},{'Name': 'instance-state-name', 'Values': ['running']},{'Name': 'tag:Name', 'Values': ['*temp*']},{'Name': 'instance-state-name', 'Values': ['running']},{'Name': 'tag:Name', 'Values': ['*test*']}])
-      for instance in instances:
-         if instance is not None:
-            if instance.tags is not None:
-               for tag in instance.tags:
-                  if tag['Key'] == 'Name':
-                     print('\033[32mName tag    \033[0m'+tag['Value'])
-               print('\033[32mID          \033[0m'+instance.id)
-               print('\033[32mType        \033[0m'+instance.instance_type)
-               if instance.public_dns_name:
-                  print('\033[32mPublic DNA  \033[0m'+instance.public_dns_name)
-               print('\033[32mRegion      \033[0m'+region)
-               print('---------------------------------------') 
-
-def show_buckets(args):
-   print('\n\033[32mShowing buckets\033[0m\n');
-   try:
-      s3 = boto3.resource('s3')
-      for bucket in s3.buckets.all():
-         print('\033[32m- \033[0m'+bucket.name)
-      print('')
-   except botocore.exceptions.ClientError:
-      print('\n\033[31mToken expired. Sorry :(\033[0m')
-      sys.exit(1)
-
-def show_ip(args):
-   print('\n\033[32mShowing Elastic IPs unused\033[0m\n')
-   try:
-      client = boto3.client('ec2')
-      regions = [region['RegionName'] for region in client.describe_regions()['Regions']]
-   except botocore.exceptions.ClientError:
-      print('\n\033[31mToken expired. Sorry\033[0m')
-      sys.exit(1)
-   for region in regions:
-      client = boto3.client('ec2', region_name=region)
-      adresses = client.describe_addresses()['Addresses']
-      for address in adresses:
-         if 'NetworkInterfaceId' not in address:
-            print(address['PublicIp']+"\033[32m in \033[0m"+region)
-
-def show_elb(args):
-   print('\n\033[32mShowing elb\033[0m\n')
-   try:
-      elb = boto3.client('elb')
-      ec2r = boto3.client('ec2')
-      lb = elb.describe_load_balancers()
-      regions = [region['RegionName'] for region in ec2r.describe_regions()['Regions']]
-   except botocore.exceptions.ClientError:
-      print('\nToken expired. Sorry :(')
-      sys.exit(1)
-   for region in regions:
-      ec2r = boto3.client('ec2', region_name=region)
-      for elbs in lb['LoadBalancerDescriptions']:
-         if len(elbs['Instances']) == 0:
-            print(elbs['LoadBalancerName']+' in '+region)
-
-def list_profiles():
-   print('\033[32mProfiles\033[0m\n')
-   sts = boto3.Session()
-   for i in sts.available_profiles:
-      print(i)
-   print('')
-
-if __name__ == '__main__':
-   test_conn(args)
-   try:
-      main(args)
-   except KeyboardInterrupt:
-      print('')
-      print('Ok, ok, quitting...')
-      sys.exit(0)
-   except botocore.exceptions.NoCredentialsError:
-      print('\033[31mIt seems your credentials are missing.\033[0m')
-      sys.exit(1)
-   except botocore.exceptions.ClientError as ex:
-      if ex.response['Error']['Code'] == 'ExpiredToken':
-         print('The token seems expired.')
-         sys.exit(1)
-   except botocore.vendored.requests.exceptions.SSLError:
-      print('Connection timed out')
-      sys.exit(1)
-
+main()
