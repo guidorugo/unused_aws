255dee1a20e0aefaf3360a5a8e26a4700cd80cee
Guido Rugo <guido.rugo@gmail.com>
2018-04-06 15:56:26 -0300

    V2.0.2 BETA - Added ELB

--- unused.py
+++ unused.py
@@ -12,25 +12,27 @@ parser.add_argument('--region', '-r', metavar='<us-east-1>,<eu-west-1>,...', nar
 parser.add_argument('--service', '-s', metavar='<ec2|rds|s3|all>', nargs='?', help='Service(s) to scrape')
 args   = parser.parse_args()
 
+def show_all():
+   list_ec2()
+   show_ip()
+   show_elb()
+
 def list_ec2():
    if args.profile:
       profiles = args.profile.split()
    else:
       profiles = boto3.Session().available_profiles
    for profile in profiles:
-      count = 0
       if (not fnmatch(profile, 'default')) and (not fnmatch(profile, '*assumed*')) and not None:
+         count = 0
          boto3.setup_default_session(profile_name=profile) 
          if not args.region:
-            ec2 = boto3.client('ec2', region_name='us-east-1') 
             regions = [region['RegionName'] for region in boto3.client('ec2', region_name='us-west-1').describe_regions()['Regions']]
          else:
-            ec2 = boto3.client('ec2', region_name=args.region)
             regions = args.region.split()
          ec2_table = PrettyTable(['\033[33mName\033[0m', '\033[33mInstance ID\033[0m', '\033[33mRegion\033[0m', '\033[33mInstance Type\033[0m', '\033[33mPublic IP\033[0m', '\033[33mState\033[0m'])
          for region in regions:
-            ec2 = boto3.resource('ec2', region_name=region)
-            instances = ec2.instances.filter()
+            instances = boto3.resource('ec2', region_name=region).instances.filter()
             for instance in instances:
                if instance is not None:
                   if instance.state['Name'] == 'pending':
@@ -65,15 +67,12 @@ def show_ip():
    else:
       profiles = boto3.Session().available_profiles
    for profile in profiles:
-      count = 0
       if (not fnmatch(profile, 'default')) and (not fnmatch(profile, '*assumed*')) and not None:
-         print ('\nProcessing profile '+profile)
+         count = 0
          boto3.setup_default_session(profile_name=profile) 
          if not args.region:
-            ec2 = boto3.client('ec2', region_name='us-east-1') 
             regions = [region['RegionName'] for region in boto3.client('ec2', region_name='us-west-1').describe_regions()['Regions']]
          else:
-            ec2 = boto3.client('ec2', region_name=args.region)
             regions = args.region.split()
          ips_table = PrettyTable(['\033[33mInstance ID\033[0m', '\033[33mPublic IP\033[0m', '\033[33mRegion\033[0m', '\033[33mStatus\033[0m'])
          for region in regions:
@@ -94,10 +93,41 @@ def show_ip():
       else:
          print('\nNo Elastic IPs on '+profile)
 
+
+def show_elb():
+   if args.profile:
+      profiles = args.profile.split()
+   else:
+      profiles = boto3.Session().available_profiles
+   for profile in profiles:
+      if (not fnmatch(profile, 'default')) and (not fnmatch(profile, '*assumed*')) and not None:
+         count = 0
+         boto3.setup_default_session(profile_name=profile)
+         if not args.region:
+            regions = [region['RegionName'] for region in boto3.client('ec2', region_name='us-east-1').describe_regions()['Regions']]
+         else:
+            regions = args.region.split()
+         elb_table = PrettyTable(['\033[33mName\033[0m', '\033[33mDNS\033[0m', '\033[33mAZs\033[0m', '\033[33mInstances\033[0m'])
+         for region in regions:
+            for elbs in boto3.client('elb', region_name=region).describe_load_balancers()['LoadBalancerDescriptions']:
+               elb_table.add_row([elbs['LoadBalancerName'], elbs['DNSName'], region, len(elbs['Instances'])])
+               count += 1
+      if count > 0:
+         print ('\n\033[93mTables in \033[0m'+profile)
+         print elb_table.get_string(sortby='\033[33mName\033[0m')
+      else:
+         print('\nNo Elastic Load Balancer on '+profile)
+
+
 def main():
    if args.service == 'ec2':
       list_ec2()
    elif args.service == 'eip':
       show_ip()
-
+   elif args.service == 'elb':
+      show_elb()
+   else:
+      print ('\nGetting all resources. This could take some time')
+      show_all()
+      
 main()
